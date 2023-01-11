47b7a9fe954ff96e12929990346f0e987915b113
Mohamed Salem <drakelogan52@gmail.com>
2023-01-11 21:07:05 +0200

    added header and product index action

--- db/migrate/20230111181331_create_products.rb
+++ db/migrate/20230111181331_create_products.rb
@@ -1,7 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.text :description
+      t.decimal :price, precision: 8, scale: 2
+      t.integer :stock
       t.timestamps
     end
   end
