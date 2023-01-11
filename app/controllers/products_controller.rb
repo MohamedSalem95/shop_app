df658e16265ccf28704ca537f09fd0fed0ed545d
Mohamed Salem <drakelogan52@gmail.com>
2023-01-11 23:15:56 +0200

    added edit and update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-	before_action :set_product, only: [:show, :destroy]
+	before_action :set_product, only: [:show, :edit, :update, :destroy]
 	def index
 		@products = Product.all
 	end
@@ -15,15 +15,26 @@ class ProductsController < ApplicationController
 		@product = Product.new(product_params)
 
 		if @product.save
-			redirect_to products_path, notice: 'Product created successfully'
+			redirect_to @product, notice: 'Product Created Successfully'
 		else
 			render :new, status: :unprocessable_entity
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @product.update(product_params)
+			redirect_to @product, notice: 'Product Updated Successfully'
+		else
+			render :edit, status: :unprocessable_entity
+		end
+	end
+
 	def destroy
 		@product.destroy
-		redirect_to products_path, notice: 'Product Deleted successfully'
+		redirect_to products_path, notice: 'Product Deleted Successfully'
 	end
 
 	private
